2153869b2d325c6d735da0fd2581ab5152987e69
manav <manavmalhotra2020@gmail.com>
2026-03-09 13:54:48 +0530

    SVG Project - Flipkart(Find cheap macmini)

---
+++ src/test/java/org/example/Tasks/svg_fk_task.java
@@ -0,0 +1,63 @@
+package org.example.Tasks;
+
+import io.qameta.allure.Description;
+import org.example.CommonToAll;
+import org.example.waitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class svg_fk_task extends CommonToAll {
+
+    //*[local-name()='svg']
+
+    @Description("")
+    @Test
+    public void test_fk_svg_icon(){
+        ChromeDriver driver = new ChromeDriver();
+        openBrowser(driver, "https://www.flipkart.com/search");
+
+        WebElement searchInputBox = driver.findElement(By.xpath("//input[@type=\"text\"]"));
+        searchInputBox.sendKeys("Macmini");
+        searchInputBox.sendKeys(Keys.ENTER);
+
+        List<WebElement> svgSearch = driver.findElements(By.xpath("//*[local-name()='svg']"));
+        svgSearch.getFirst().click();
+
+        waitHelpers.waitForVisibility(
+                driver,10, "//div[contains(@data-id,\"CPU\")]"
+        );
+
+        List<WebElement> allPrices = driver.findElements(By.xpath(
+                "//a[contains(text(),\"Apple\")]/../div/../a[3]//div[@class=\"hZ3P6w\"]"
+        ));
+
+        int min = Integer.MAX_VALUE;
+
+
+//        System.out.println(allTitles.getFirst().getText());
+        for (WebElement price : allPrices) {
+            String cp = price.getText();
+            cp = cp.replace("₹", "").replace(",","");
+            System.out.println(cp);
+            try {
+                int current_price = Integer.parseInt(cp);
+                if (current_price < min) {
+                    min = current_price;
+                }
+            }catch (NumberFormatException e){
+                continue;
+            }
+        }
+
+        System.out.println("Cheapest mac price available is: "+ min);
+
+
+
+        closeBrowser(driver);
+    }
+}
